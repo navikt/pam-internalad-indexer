a7a6f996ce73534fc2fe2cfc2b1833d4fce984af
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2020-01-08 08:48:50 +0100

    added administration support

--- src/main/kotlin/no/nav/arbeidsplassen/internalad/indexer/feed/AdTransport.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/internalad/indexer/feed/AdTransport.kt
@@ -1,13 +1,15 @@
 package no.nav.arbeidsplassen.internalad.indexer.feed
 
 import java.time.LocalDateTime
+import java.util.*
 
 data class AdTransport(val id: Long, val uuid: String, val createdBy: String, val updatedBy: String, val created: LocalDateTime,
                        val updated: LocalDateTime, val contactList: List<Contact> = listOf(), val mediaList: List<Media> = listOf(),
                        val locationList: List<Location> = listOf(), val properties: Map<String, String> = hashMapOf(),
                        val title: String, val status: String, val privacy: String, val source: String,
                        val medium: String, val reference: String, val published: LocalDateTime?, val expires: LocalDateTime,
-                       val employer: AdCompany?, val categoryList: List<Category> = listOf(), val businessName: String?)
+                       val employer: AdCompany?, val categoryList: List<Category> = listOf(), val businessName: String?,
+                       val administration: Administration)
 
 data class Category(val id: Long, val code: String, val categoryType: String, val name: String, val description: String?, val parentId: Long?)
 
@@ -25,3 +27,6 @@ data class AdCompany(val id: Long, val uuid: String, val createdBy: String, val 
                      val locationList: List<Location> = listOf(), val properties: Map<String, String> = hashMapOf(),
                      val name: String, val orgnr: String?, val status: String, val parentOrgnr: String?,
                      val publicName: String?, val deactivated: LocalDateTime?, val orgform: String?, val employees: Int)
+
+data class Administration(val status: String, val comments: String?, val reportee: String?,
+                          val remarks :List<String> = listOf(), val navIdent: String?)
