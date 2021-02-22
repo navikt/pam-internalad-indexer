6d9f9acc9090266a76187c8791d573717b1849af
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2021-02-22 12:26:59 +0100

    fix bug kafka config

--- scripts/init-kafka-env.sh
+++ scripts/init-kafka-env.sh
@@ -3,3 +3,5 @@ export KAFKA_SSL_TRUSTSTORE_PASSWORD=${NAV_TRUSTSTORE_PASSWORD}
 export KAFKA_SASL_JAAS_CONFIG="org.apache.kafka.common.security.plain.PlainLoginModule required username=\"${SERVICEUSER}\" password=\"${SERVICEUSER_PASSWORD}\";"
 export KAFKA_SASL_MECHANISM=PLAIN
 export KAFKA_SECURITY_PROTOCOL=SASL_SSL
+export KAFKA_HEALTH_ENABLED=false
+
