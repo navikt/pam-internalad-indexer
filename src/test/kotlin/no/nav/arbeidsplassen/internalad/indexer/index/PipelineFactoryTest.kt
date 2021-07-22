99a44ff7e0a6902f369de7569bc8dfebaddee1eb
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2021-07-22 10:01:57 +0200

    fix a test

--- src/test/kotlin/no/nav/arbeidsplassen/internalad/indexer/index/PipelineFactoryTest.kt
+++ src/test/kotlin/no/nav/arbeidsplassen/internalad/indexer/index/PipelineFactoryTest.kt
@@ -3,6 +3,7 @@ package no.nav.arbeidsplassen.internalad.indexer.index
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import no.nav.arbeidsplassen.internalad.indexer.process.PipelineFactory
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 @MicronautTest
@@ -12,8 +13,8 @@ class PipelineFactoryTest(private val adPipelineFactory: PipelineFactory, privat
     fun pipelineFactoryTest() {
         val ad = objectMapper.readValue(PipelineFactoryTest::class.java.getResourceAsStream("/fullAdDTO.json"), AdTransport::class.java)
         val items = adPipelineFactory.toPipelineStream(listOf(ad))
-        items.forEach {
-            println(objectMapper.writeValueAsString(it))
-        }
+        val item = items.findFirst().get()
+        println(objectMapper.writeValueAsString(item.document))
+        Assertions.assertNotNull(item.document.get("properties").get("_score"))
     }
 }
